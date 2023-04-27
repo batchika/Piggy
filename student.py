a8ccfb687a601c4b8aa41478c8602ca5bd54b3ec
batchika <125601333+batchika@users.noreply.github.com>
2023-04-27 18:15:08 +0000

    trun

--- student.py
+++ student.py
@@ -62,7 +62,7 @@ class Piggy(PiggyParent):
         time.sleep(2)
         self.stop()
         self.right()
-        time.sleep(0.95)
+        time.sleep(0.90)
         self.stop()
       
     def dance(self):

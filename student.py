d8ef81877b58d4aff5b69a6f1660c60d183d8fc6
batchika <125601333+batchika@users.noreply.github.com>
2023-04-27 18:20:53 +0000

    self.scan stuff

--- student.py
+++ student.py
@@ -64,7 +64,10 @@ class Piggy(PiggyParent):
         self.right()
         time.sleep(0.90)
         self.stop()
-      
+      self.scan()
+      print(self.scan_data)
+
+  
     def dance(self):
         """A higher-ordered algorithm to make your robot dance"""
         # TODO: check to see if it's safe before dancing

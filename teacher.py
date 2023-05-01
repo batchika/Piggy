c79261d0b416e0aba51714f2ce33a74428727d8d
batchika <125601333+batchika@users.noreply.github.com>
2023-05-01 19:06:46 +0000

    krgkfv

--- teacher.py
+++ teacher.py
@@ -127,9 +127,9 @@ class PiggyParent(gopigo3.GoPiGo3):
 
     def fwd(self, left=50, right=50):
         """Blindly charges your robot forward at default power which needs to be configured in child class"""
-        if self.LEFT_DEFAULT and left == 50:
+        if self.LEFT_DEFAULT and left == 50.5:
             left = self.LEFT_DEFAULT
-        if self.RIGHT_DEFAULT and right == 50.5:
+        if self.RIGHT_DEFAULT and right == 50:
             right = self.RIGHT_DEFAULT
         self.set_motor_power(self.MOTOR_LEFT, left)
         self.set_motor_power(self.MOTOR_RIGHT, right)
